1506a9a0aec560fc5bce3acc1a62b6e9c763e4b0
박성준 <freestarjun@gmail.com>
2025-08-05 12:32:30 +0900

    feat : 광고 이미지의 커러셀 제작

--- mission/script.js
+++ mission/script.js
@@ -1,113 +1,26 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let current = 0;
-  const slides = document.querySelectorAll(".slider a");
+const slider = document.querySelector('.slider');
+const images = document.querySelectorAll('.slider img');
+const leftBtn = document.querySelector('.left-btn');
+const rightBtn = document.querySelector('.right-btn');
 
-  function showSlide(index) {
-    slides.forEach((slide, i) => {
-      slide.classList.remove("active");
-      if (i === index) {
-        slide.classList.add("active");
-      }
-    });
-  }
+let index = 0;
+const total = images.length;
 
-  function nextSlide() {
-    current = (current + 1) % slides.length;
-    showSlide(current);
-  }
+function updateSlider() {
+  slider.style.transform = `translateX(-${index * 100}%)`;
+}
 
-  showSlide(current);
-  setInterval(nextSlide, 3000);
+leftBtn.addEventListener('click', () => {
+  index = (index - 1 + total) % total;
+  updateSlider();
 });
 
-
-const datas = [
-  {
-    id: 1,
-    img: "kickboard.png",
-    name: "위페드 미니9 전동킥보드",
-    price: 1100000,
-    date: 3
-  },
-  {
-    id: 2,
-    img: "tire.png",
-    name: "미쉐린 A급 중고타이어",
-    price: 300000,
-    date: 1
-  },
-  {
-    id: 3,
-    img: "hoodie.png",
-    name: "베이프 크롭 녹계 후드 집업",
-    price: 336000,
-    date: 1
-  },
-  {
-    id: 4,
-    img: "bicycle.png",
-    name: "언노운 sl82 리어 픽시",
-    price: 300000,
-    date: 4
-  },
-  {
-    id: 5,
-    img: "iphone.png",
-    name: "아이폰 16 프로맥스",
-    price: 1013000,
-    date: 1
-  },
-  {
-    id: 6,
-    img: "polo.png",
-    name: "90s 폴로 랄프로텐 CALDWELL 쿠바 구아야베 셔츠",
-    price: 104000,
-    date: 5
-  },
-  {
-    id: 7,
-    img: "golf.png",
-    name: "브리지스톤 V300-9 아이언세트",
-    price: 10200000,
-    date: 3
-  },
-  {
-    id: 8,
-    img: "gold.png",
-    name: "까르띠에 롱드머스트 36미리 금목걸이",
-    price: 5300000,
-    date: 9
-  },
-  {
-    id: 9,
-    img: "bmw.png",
-    name: "BMW s1000rr 15년식",
-    price: 8300000,
-    date: 7
-  },
-  {
-    id: 10,
-    img: "victor.png",
-    name: "빅터 팬텀 4u",
-    price: 180000,
-    date: 3
-  }
-];
-
-const container = document.getElementById("product-list");
-
-datas.map(item => {
-  const div = document.createElement("div");
-  div.className = "product";
-
-  const priceWithComma = item.price.toLocaleString();
-
-  div.innerHTML = `
-    <img src="${item.img}" alt="${item.name}">
-    <div class="product-name">${item.name}</div>
-    <div class="product-price">${priceWithComma}원</div>
-    <div class="product-date>${item.date}일 전</div>
-  `;
-
-  container.appendChild(div);
+rightBtn.addEventListener('click', () => {
+  index = (index + 1) % total;
+  updateSlider();
 });
+
+setInterval(() => {
+  index = (index + 1) % total;
+  updateSlider();
+}, 5000);

1c52d3a30b2a8cbdbcf633bc11fcdd1ab6c5952e
leegyumin <leebadagyumin0129@gmail.com>
2025-08-11 11:00:36 +0900

    refactor : 카테고리 목록 요소를 js를 통해 동적으로 추가하게 변경

--- mission/script.js
+++ mission/script.js
@@ -52,3 +52,37 @@ const move_top = document.getElementById("move-top");
 move_top.addEventListener("click", () => {
     window.scrollTo({top: 0, behavior: 'smooth'});
 })
+
+const category_data = [
+    "여성의류",
+    "남성의류",
+    "신발",
+    "가방/지갑",
+    "시계",
+    "쥬얼리",
+    "패션 액세서리",
+    "디지털",
+    "가전제품",
+    "스포츠/레저",
+    "차량/오토바이",
+    "스타굿즈",
+    "키덜트",
+    "예술/희귀/수집품",
+    "음반/악기",
+    "도서/티켓/문구",
+    "뷰티/미용",
+    "가구/인테리어",
+    "생활/주방용품",
+    "공구/산업용품",
+    "식품",
+    "유아동/출산",
+    "반려동물용품",
+    "기타",
+    "재능"
+]
+
+const category_list = document.getElementById("categorys-list");
+
+category_data.map((x) => {
+    category_list.innerHTML += `<li>${x}</li>`;
+})
